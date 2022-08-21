10b203427187058f0035b393bcea40140423284d
manish30007 <70449659+manish30007@users.noreply.github.com>
2022-08-21 11:34:30 +0530

    New.js change

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ export default class App extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
       <Router>
        <Navbar/>
        <Switch>
@@ -23,20 +22,6 @@ export default class App extends Component {
         <Route exact path="/science" key="science" ><News pageSize={this.pageSize} country="in" category="science"/></Route>
         <Route exact path="/sports" key="sports" ><News pageSize={this.pageSize} country="in" category="sports"/></Route>
         <Route exact path="/technology" key="technology"  ><News pageSize={this.pageSize} country="in" category="technology"/></Route>
-=======
-        <Router>
-       <Navbar/>
-       <Switch>
-        <Route exact path="/" key="home"  ><News pageSize={5} country="in" category="general"/></Route>
-        <Route exact path="/about" key="about"  ><News pageSize={5} country="in" category="general"/></Route>
-        <Route exact path="/general" key="general"  ><News pageSize={5} country="in" category="general"/></Route>
-        <Route exact path="/business" key="business" ><News pageSize={5} country="in" category="business"/></Route>
-        <Route exact path="/entertainment" key="entertainment" ><News pageSize={5} country="in" category="entertainment"/></Route>
-        <Route exact path="/health" key="health" ><News pageSize={5} country="in" category="health"/></Route>
-        <Route exact path="/science" key="science" ><News pageSize={5} country="in" category="science"/></Route>
-        <Route exact path="/sports" key="sports" ><News pageSize={5} country="in" category="sports"/></Route>
-        <Route exact path="/technology" key="technology"  ><News pageSize={5} country="in" category="technology"/></Route>
->>>>>>> 26397d0cdcfed9ea850a8833941d47232018e9da
        </Switch>
        </Router>
       </div>
